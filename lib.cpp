559dd77fc79742a06985e36bf480cc50fed65a3f
MatteoDerchi <91695814+MatteoDerchi@users.noreply.github.com>
2021-11-30 14:37:55 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+//
+// Created by Matteo on 30/11/2021.
+//
 
+#include "lib.h"
+bool numeroprimo(int a){
+    bool t;
+    for(int i=2;i<a;i++){
+        if (a%i != 0){
+            t= true;
+            return (t);
+        }else{
+            t=false;
+            return (t);
+        }
+    }
+}
