1dc59b6fdbd32f39ba1a4aac50d3a196e38394a2
MatteoDerchi <91695814+MatteoDerchi@users.noreply.github.com>
2021-11-30 19:37:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+int main() {
+    int a=0;
+    cin>>a;
+    if (numeroprimo(a)==true){
+        cout<< "numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
+    return 0;
 }
